7019ea46b0163a7023cc995141fc41ae73fff2ea
becket <becket.bowes@gmail.com>
2022-05-03 05:22:25 -0400

    i think we can all admit that mutations were made

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schema');
+const { authMiddleware } = require('./utils/auth')
 
 const PORT = process.env.PORT || 3033;
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, context: authMiddleware });
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
